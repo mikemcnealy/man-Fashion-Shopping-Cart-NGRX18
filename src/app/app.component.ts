7bc1aa1885cc0e2a3bbfbc4a88fa50876bd615b5
Michael McNealy <mike.mcnealy@outlook.com>
2024-08-19 12:55:00 -0400

    update project folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from '../app/layout/header/header.component';
-import {CartComponent} from '../app/cart/cart.component'
+import {CartComponent} from '../app/cart/cart.component';
+import {FoolerComponent} from '../app/layout/fooler/fooler.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,CartComponent],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    CartComponent,
+    FoolerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
